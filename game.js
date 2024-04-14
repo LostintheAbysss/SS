11c708f5639015690c21fabeb709da5250e98f23
Roger <122247375+LostintheAbysss@users.noreply.github.com>
2024-04-14 18:40:06 +0530

    Update game.js
    v.0.2.4

--- game.js
+++ game.js
@@ -2,9 +2,12 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 var userClickedPattern = [];
-
+ 
 var started = false;
 var level = 0;
+let score = 0;
+let highScore = localStorage.getItem("highScore");
+var isGameOver = false; 
 retry.style.display = "none";
 
 $(document).on('keypress touchstart', function () {
@@ -12,12 +15,15 @@ $(document).on('keypress touchstart', function () {
         $("#level-title").text("Level " + level);
         nextSequence();
         started = true;
-
+        isGameOver = false; 
+        
     }
 
 });
 
-$(".btn").click(function () {
+$(".btn").on("mousedown touchstart" ,function () {
+    if (isGameOver) return;
+
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
@@ -27,7 +33,8 @@ $(".btn").click(function () {
     checkAnswer(userClickedPattern.length - 1);
 });
 
-function checkAnswer(currentLevel) {
+function checkAnswer(currentLevel) {  
+    if (isGameOver) return;
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
         console.log("success");
@@ -39,7 +46,6 @@ function checkAnswer(currentLevel) {
             }, 1000);
 
         }
-
     } else {
         playSound("wrong")
 
@@ -49,24 +55,37 @@ function checkAnswer(currentLevel) {
         }, 200);
 
         console.log("wrong");
-        let score = 0;
+        //score 
         if (level > 0){
             score = level - 1;
+        }  
+
+        //high score 
+        if (highScore === null || score > parseInt(highScore)){
+            highScore = localStorage.setItem("highScore", score);
+            highScore = score;
         }
+        
+        let displayHighScore = localStorage.getItem("highScore");
+               
+        
    
-        $("#level-title").html(" <span id='over'>Game Over!</span> <br><span id='restart'>Tap to Restart </span> <br> <span id='score'>Score:" +"</span> <br>"+ score  );
-        $("#score").css("color", "black");
+        $("#level-title").html(" <span id='over'>Game Over!</span> <br><span id='restart'>Tap to Restart </span> <br> <span id='score'>Score:" +"</span> <br> "+ score + " <br> <span id='hs'>HS</span>:" +"<span id='HSNumber'>" + displayHighScore + "</span>"  );
         $("#restart").css("font-size", "0.5em");
-        //retry button
-        retry.style.display = "inline";
+
+        retry.style.display = "inline";          
         $("#retry").click(function(){
-            location.reload();
-        })
-    }
+            setTimeout(function () {
+                location.reload();
+            }, 50);
+
+        }) 
+        isGameOver = true; 
+    } 
 }
 function nextSequence() {
+   
     userClickedPattern = [];
-
     var randomNumber = Math.floor(Math.random() * 4)
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
@@ -76,14 +95,14 @@ function nextSequence() {
 
     level++;
     $("#level-title").text("Level " + level)
-
+     
 
 }
 
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
-}
+}  
 
 function animatePress(currentColour) {
     $("#" + currentColour).addClass("pressed");
